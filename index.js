f135b0855c6a5dc9ce84cedb0f2747d158970161
JustinW1233 <79864211+JustinW1233@users.noreply.github.com>
2021-05-26 10:34:01 -0600

    added to createAccount.pug

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-const dbPath = "mongodb+srv://JWilliams1233:9542715.Jw1233@cluster0.ujp1m.mongodb.net/Users?retryWrites=true&w=majority"
+const express = require('express');
+const pug = require('pug');
+const routes = require('./routes/routes');
+const path = require('path');
 
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
+const app = express();
 
-mongoose.connect(dbPath, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-});
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-let mdb = mongoose.connection;
-mdb.on('error', console.error.bind(console, 'connection error'));
-
-//pp poo poo
-//Ya boi
-//Aww yea B)
+app.listen(3000);
